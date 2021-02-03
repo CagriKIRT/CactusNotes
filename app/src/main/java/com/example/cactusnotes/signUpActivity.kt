1eee5334a4e20c237dc447f6b1c6f8cca5386665
cagrikirt <cagrikirt@gmail.com>
2021-02-03 10:36:15 +0300

    Implement view binding

--- app/src/main/java/com/example/cactusnotes/signUpActivity.kt
+++ app/src/main/java/com/example/cactusnotes/signUpActivity.kt
@@ -2,11 +2,18 @@ package com.example.cactusnotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cactusnotes.databinding.SignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: SignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.sign_up)
+        binding=SignUpBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+
         supportActionBar?.title = getString(R.string.signUp_actionBar)
     }
 }
